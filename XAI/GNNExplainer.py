6f22ba7a41aa2a5bc528d8362c8a453dd2b146c7
melinaem <62059573+melinaem@users.noreply.github.com>
2022-11-21 17:13:25 +0200

    Update GNNExplainer.py

--- XAI/GNNExplainer.py
+++ XAI/GNNExplainer.py
@@ -26,3 +26,76 @@ data.train_mask[:data.num_nodes - 1000] = 1
 data.val_mask = None
 data.test_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
 data.test_mask[data.num_nodes - 500:] = 1
+
+
+# Build model
+
+class Net(torch.nn.Module):
+    def __init__(self, num_features, dim=16, num_classes=1):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(num_features, dim)
+        self.conv2 = GCNConv(dim, num_classes)
+
+    def forward(self, x, edge_index, data=None):
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+      
+# Training
+
+epochs = 200
+dim = 16
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net(num_features=train_dataset.num_features, dim=dim, num_classes=train_dataset.num_classes).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-3)
+
+
+# Testing
+
+def test(model, data):
+    model.eval()
+    logits, accs = model(data.x, data.edge_index, data), []
+    for _, mask in data('train_mask', 'test_mask'):
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+        accs.append(acc)
+    return accs
+  
+loss = 999.0
+train_acc = 0.0
+test_acc = 0.0
+
+t = trange(epochs, desc="Stats: ", position=0)
+
+for epoch in t:
+
+    model.train()
+    
+    loss = 0
+
+    data = data.to(device)
+    optimizer.zero_grad()
+    log_logits = model(data.x, data.edge_index, data)
+
+    # Since the data is a single huge graph, training on the training set is done by masking the nodes that are not in the training set.
+    loss = F.nll_loss(log_logits[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+    # validate
+    train_acc, test_acc = test(model, data)
+    train_loss = loss
+    
+    t.set_description('[Train_loss:{:.6f} Train_acc: {:.4f}, Test_acc: {:.4f}]'.format(loss, train_acc, test_acc))
+    
+ # Explainer
+
+node_idx = 10
+x, edge_index = data.x, data.edge_index
+explainer = GNNExplainer(model, epochs=200)
+node_feat_mask, edge_mask = explainer.explain_node(node_idx, x, edge_index)
+
+ax, G = explainer.visualize_subgraph(node_idx, edge_index, edge_mask, y=data.y)
+plt.show()
+
